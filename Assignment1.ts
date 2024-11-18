460aaf574daaf3207bd592fbac17ebe348568681
venishk <venish8@gmail.com>
2024-11-19 00:19:32 +0500

    Add files via upload

---
+++ Assignment1.ts
@@ -0,0 +1,26 @@
+function myfunc2(){
+    let name1= "venish"
+    console.log(name1)
+
+    function class1(){
+        let class1="Governor house"
+        console.log(class1)
+
+        function employer(){
+            let employer="Samba Bank"
+            console.log(employer)
+
+            function timing(){
+                let timing="Friday 7pm to 10pm"
+                console.log(timing)
+
+            }
+            timing()
+            
+        }
+        employer()
+    
+    }
+    class1()
+}
+myfunc2()
---
+++ assignment1.js
@@ -0,0 +1,20 @@
+function myfunc2() {
+    var name1 = "venish";
+    console.log(name1);
+    function class1() {
+        var class1 = "Governor house";
+        console.log(class1);
+        function employer() {
+            var employer = "Samba Bank";
+            console.log(employer);
+            function timing() {
+                var timing = "Friday 7pm to 10pm";
+                console.log(timing);
+            }
+            timing();
+        }
+        employer();
+    }
+    class1();
+}
+myfunc2();
